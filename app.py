5ffdc33735a798e4d2c1b161951ce2950931ddb2
Raphael Schwingel <raphael@mentorstec.com.br>
2022-01-25 14:13:18 -0300

    confirmo

--- app.py
+++ app.py
@@ -14,36 +14,36 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class InsertDB(db.Model):
+class CredencialPBI(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_cliente = db.Column(db.String(100))
-    user_name = db.Column(db.String(100))
-    pw_cliente = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
 
 @app.route('/')
 def home():
-    users = InsertDB.query.all()
+    users = CredencialPBI.query.all()
     return render_template("gc.html", users=users)  
 
 @app.route("/add", methods=["POST"])
 def add():
     id_cliente = request.form.get("id_cliente")
-    user_name = request.form.get("user_name")
-    pw_cliente = request.form.get("pw_cliente")
-    senha_cript = cryptocode.encrypt(pw_cliente, senha_padrao) 
-    new_cliente = InsertDB(id_cliente=id_cliente, user_name=user_name, pw_cliente=senha_cript)
+    username = request.form.get("username")
+    password = request.form.get("password")
+    senha_cript = cryptocode.encrypt(password, senha_padrao) 
+    new_cliente = CredencialPBI(id_cliente=id_cliente, username=username, password=senha_cript)
     db.session.add(new_cliente)
     db.session.commit()
     return redirect(url_for("home"))
 
 @app.route("/edit/<int:id>", methods =['GET', 'POST'])
 def edit(id):
-    cliente = InsertDB.query.filter_by(id=id).first()
+    cliente = CredencialPBI.query.filter_by(id=id).first()
     if request.method == 'POST':
         cliente.id_cliente = request.form['id_cliente']
-        cliente.user_name = request.form['user_name']
-        senha_antiga = request.form['pw_cliente']
-        cliente.pw_cliente = cryptocode.encrypt(senha_antiga, senha_padrao)  
+        cliente.username = request.form['username']
+        senha_antiga = request.form['password']
+        cliente.password = cryptocode.encrypt(senha_antiga, senha_padrao)  
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("edit.html", users=cliente)
@@ -51,7 +51,7 @@ def edit(id):
 
 @app.route("/delete/<int:id_cliente>")
 def delete(id_cliente):
-    cliente = InsertDB.query.filter_by(id=id_cliente).first()
+    cliente = CredencialPBI.query.filter_by(id=id_cliente).first()
     db.session.delete(cliente)
     db.session.commit()
     return redirect(url_for("home"))
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
                             backref)
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///db_clientes_mentorstec.sqlite')
+engine = create_engine('sqlite:///db/db_clientes_mentorstec.sqlite')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -15,5 +15,5 @@ class InsertDB(Base):
     __tablename__ = 'clientesMentors'
     id = Column(Integer, primary_key=True)
     id_cliente = Column(String)
-    user_name = Column(String)
-    pw_cliente = Column(String)
+    username = Column(String)
+    password = Column(String)
